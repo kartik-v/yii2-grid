b767caca8638f8d02f1d23e3838edba40d107dba
Kartik Visweswaran <kartikv2@gmail.com>
2014-03-29 19:42:18 +0530

    Added nowrap setting for columns

--- grid/DataColumn.php
+++ grid/DataColumn.php
@@ -37,10 +37,10 @@ class DataColumn extends \yii\grid\DataColumn
 	public $vAlign;
     
 	/**
-	 * @var string whether to force no wrapping on all table cells in the column
+	 * @var boolean whether to force no wrapping on all table cells in the column
 	 * @see http://www.w3schools.com/cssref/pr_text_white-space.asp
 	 */
-	public $noWrap;
+	public $noWrap = false;
 
 	/**
 	 * @var string the width of each column (matches the CSS width property).
