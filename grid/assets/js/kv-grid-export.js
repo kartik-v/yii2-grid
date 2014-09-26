4d912753d1d84e91fbf5ae247db5c0ed96744975
Florian Ruppel <ruppel.florian@gmail.com>
2014-09-26 15:51:25 +0200

    Update kv-grid-export.js

--- grid/assets/js/kv-grid-export.js
+++ grid/assets/js/kv-grid-export.js
@@ -51,6 +51,7 @@ function replaceAll(str, from, to) {
         '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel"' +
             'xmlns="http://www.w3.org/TR/REC-html40">' +
             '<head>' +
+            '<meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>' +
             '{css}' +
             '<!--[if gte mso 9]>' +
             '<xml>' +
