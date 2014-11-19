ce9e65a4e44d5f0b92ed19436b29ca85dd2b719a
unknown <PELISSET@CZC2106G8B.oisehabitat.loc>
2014-11-19 09:22:12 +0100

    Added French Translation

--- messages/config.php
+++ messages/config.php
@@ -7,7 +7,7 @@ return [
     'messagePath' => __DIR__,
     // array, required, list of language codes that the extracted messages
     // should be translated to. For example, ['zh-CN', 'de'].
-    'languages' => ['de', 'en', 'hu', 'it', 'nl', 'pl', 'pt', 'ru'],
+    'languages' => ['de', 'en', 'fr', 'hu', 'it', 'nl', 'pl', 'pt', 'ru'],
     // string, the name of the function for translating messages.
     // Defaults to 'Yii::t'. This is used as a mark to find the messages to be
     // translated. You may use a string for single function name or an array for
