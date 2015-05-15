d7c7f74a9de79929edc51822daf31333abb66dbb
Kartik Visweswaran <kartikv2@gmail.com>
2015-05-15 21:49:14 +0530

    Updated translations config

--- messages/config.php
+++ messages/config.php
@@ -7,7 +7,7 @@ return [
     'messagePath' => __DIR__,
     // array, required, list of language codes that the extracted messages
     // should be translated to. For example, ['zh-CN', 'de'].
-    'languages' => ['da', 'de', 'en', 'es', 'fr', 'hu', 'it', 'nl', 'pl', 'pt', 'ru', 'vi'],
+    'languages' => ['da', 'de', 'el-GR', 'en', 'es', 'fa-IR', 'fr', 'hu', 'it', 'lt', 'nl', 'pl', 'pt', 'ru', 'vi', 'zh'],
     // string, the name of the function for translating messages.
     // Defaults to 'Yii::t'. This is used as a mark to find the messages to be
     // translated. You may use a string for single function name or an array for
