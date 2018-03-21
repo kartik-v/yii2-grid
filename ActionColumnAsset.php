5ca5d69e2d11a958a0e97a4012c6f158f8d0acb8
Kartik Visweswaran <kartikv2@gmail.com>
2018-03-21 16:20:50 +0530

    Fix #781: Add deprecated support for `ActionColumnAsset`

---
+++ ActionColumnAsset.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @package   yii2-grid
+ * @author    Kartik Visweswaran <kartikv2@gmail.com>
+ * @copyright Copyright &copy; Kartik Visweswaran, Krajee.com, 2014 - 2018
+ * @version   3.1.8
+ */
+
+namespace kartik\grid;
+
+use kartik\base\AssetBundle;
+
+/**
+ * Asset bundle for [[ActionColumn]] - deprecated since v3.1.6
+ *
+ * @author Kartik Visweswaran <kartikv2@gmail.com>
+ * @since 1.0
+ */
+class ActionColumnAsset extends AssetBundle
+{
+}
